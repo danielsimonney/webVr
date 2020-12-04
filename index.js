3475dcfe56e92c9524983cce842cf0c61c12f385
danielsimonney <daniel.simonney60@gmail.com>
2020-12-04 14:46:55 +0100

    fix direction bug

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ AFRAME.registerComponent("listener", {
     if(verifPos(posCam,posPlayer)){
       console.log("kkk")
       // if(checkIfCabane(posCam,posPlayer)){
-        let newP = this.el.components.camera.camera.parent.position.add(this.el.components.camera.camera.getWorldDirection().multiplyScalar(this.data.stepFactor));
+        let newP = this.el.components.camera.camera.parent.position.add(posCam.multiplyScalar(this.data.stepFactor));
         this.el.setAttribute('position', newP)
       // this.el.components.camera.camera.parent.position.add(posCam.multiplyScalar(this.data.stepFactor));
         // }
